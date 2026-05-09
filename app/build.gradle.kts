9f80b680abe74d229cf90859637583013bfe1789
Nikita22007 <game.nikita22007@yandex.ru>
2026-05-10 01:43:58 +0300

    ci: add ShadowJar support and make native pipeline resilient with continue-on-error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("buildlogic.kotlin-application-conventions")
     kotlin("plugin.serialization") version "2.1.0"
     id("org.graalvm.buildtools.native") version "0.10.1"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 dependencies {
